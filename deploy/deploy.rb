55c9a77de7cb5ea43014d45ded92ae8199fd7d6b
Pedro Cavalheiro <pedrohc@gmail.com>
2016-06-28 00:28:36 -0300

    some code cleanup

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -1,4 +1,5 @@
-require 'pp'
+#!/usr/bin/env ruby
+
 require 'rest-client'
 require 'colorize'
 require 'progress_bar'
@@ -6,13 +7,13 @@ require 'redis'
 require 'slop'
 require 'wannabe_bool'
 
-redis = Redis.new(:host => "redis")
+redis = Redis.new(host: 'redis')
 
 @opts = Slop.parse do |o|
   o.bool '-b', '--build', 'build'
 end
 
-available_upstreams = ['green', 'blue']
+available_upstreams = %w(green blue)
 docker_network = 'deployment'
 
 current_upstream = redis.get('current_upstream') || 'green'
@@ -20,36 +21,37 @@ current_upstream = redis.get('current_upstream') || 'green'
 new_upstream = current_upstream
 new_upstream = available_upstreams.sample while new_upstream == current_upstream
 
-puts; puts '#####################'
-print ' Current app is: '
-puts current_upstream.to_s.yellow
-print ' New app is: '
-puts new_upstream.to_s.red
-puts '#####################'
+puts "\n#####################
+Current app is: #{current_upstream.to_s.send(current_upstream)}
+New app is: #{new_upstream.to_s.send(new_upstream)}
+#####################\n\n"
 
 network_exists = `docker network ls | grep #{docker_network} | wc -l`.to_b
 unless network_exists
-  puts; puts ' Creating docker network'.black.on_yellow
+  puts 'Creating docker network'.black.on_yellow; puts
   `docker network create deployment`
 end
 
 container_is_running = `docker ps -a -q --filter="name=app-#{new_upstream}" | wc -l`.to_b
 if container_is_running
-  puts; puts ' Stopping running container'.black.on_yellow
+  print 'Stopping running container'.black.on_yellow; puts
   system("docker stop $(docker ps -a -q --filter='name=app-#{new_upstream}')")
 end
 
 if @opts[:build]
-  puts; puts ' Rebuilding container, please wait a few moments'.black.on_yellow
+  print 'Rebuilding container, please wait a few moments'.black.on_yellow
   system("docker build -t app-#{new_upstream} ../server-#{new_upstream}") || raise('Error building containers')
 end
 
-puts; puts " Starting updated container on container #{new_upstream}".black.on_yellow
-system("docker rm app-#{new_upstream}; docker run --name=app-#{new_upstream} --net=#{docker_network} -d app-#{new_upstream} ") || raise('Error starting container cloud')
+puts; puts 'Removing old container'.black.on_yellow
+system("docker rm app-#{new_upstream}")
+
+puts; puts 'Starting updated application'.black.on_yellow
+system "docker run --name=app-#{new_upstream} --net=#{docker_network} -d app-#{new_upstream} " || raise('Error starting container cloud')
 sleep 10
 
-puts; puts 'Container deployed!'.on_green
+puts; puts 'Container deployed!'.black.on_green
 
-puts; puts 'Changing upstream'.light_yellow.on_green
+puts; puts 'Changing upstream'.black.on_light_blue
 redis.set('current_upstream', new_upstream)
 puts; puts 'Done!'.light_green.on_blue
